a9ecbd60aeebf20cedf9d1d64b8bee1ea4369612
alioup <7yuriy7@gmail.com>
2016-04-27 11:26:26 -0500

    minor changes

--- src/mastermind/Driver.java
+++ src/mastermind/Driver.java
@@ -23,7 +23,7 @@ public class Driver
 	        	while(doneFlag == false)
 	        	{
 	        		try {
-		        		System.out.println("\n" + board.printBoard(true) + "\nThe available colors are: " + Arrays.toString(Peg.values()) + "\n");
+	        			System.out.println("\n" + board.printBoard(true) + "\nThe available colors are: " + Arrays.toString(Peg.values()) + "\n");
 		        		System.out.print("Enter your guess: ");
 		        		input = reader.readLine().trim().toLowerCase();
 		        		if (input.length() != 4) {
